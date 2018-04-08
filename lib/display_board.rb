90892ef658099e4c91689d0cb21ca2cbab9d2889
Afrika Williams <afrika.williams@gmail.com>
2018-04-08 20:02:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board =["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ",]
+ 
 # out the current state.
